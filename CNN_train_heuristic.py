031f5ddd8a8c0eeaa2569d215397a891da919e49
lqhung2001 <lqhung2001@gmail.com>
2022-06-14 18:10:17 +0700

    update log and res

--- CNN_train_heuristic.py
+++ CNN_train_heuristic.py
@@ -1,5 +1,19 @@
 import numpy as np
 import pandas as pd
 import os
-x_input= np.genfromtxt("log.txt",delimiter=" ")
-y_output=np.genfromtxt("res.txt",delimiter=" ")
+import torch
+import torchvision
+from torch.utils.data import DataLoader,Dataset
+
+f=open("log.txt","r")
+x_input=np.zeros((0,20,20))
+for i in range(2):
+    x=""
+    for i in  range(20):
+        x=x+f.readline()
+    print(x)
+    cur_input=np.fromstring(x,sep=' ')
+    print(cur_input)
+    cur_input.reshape((20,20))
+    print(cur_input.shape)
+    x=np.append(x,cur_input,axis=0)
